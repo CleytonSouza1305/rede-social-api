f8216b999da8aae2a11a777b6af9815c4b9ef1d3
Cleyton de Jesus Conceição Souza <141965244+CleytonSouza1305@users.noreply.github.com>
2025-01-27 11:55:43 -0300

    Adicionando cors ao projeto.

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,10 +4,12 @@ const express = require('express')
 const authRouter = require('./routes/authroures')
 const errorMiddeware = require('./middleware/error-middeware')
 const postRouter = require('./routes/postRoutes')
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
+app.use(cors)
 app.use('/auth', authRouter)
 app.use('/post', postRouter)
 
